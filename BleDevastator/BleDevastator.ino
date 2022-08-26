c8dd20ffd2e021534f5e4fb3f1ee5d62e3d1cd01
ds <dannysilence@homelabs.company>
2022-08-26 07:37:50 +0300

    added ble devastator

--- BleDevastator/BleDevastator.ino
+++ BleDevastator/BleDevastator.ino
@@ -10,22 +10,11 @@ SoftwareSerial Serial1(2, 3);
 
 #define BLOCK_FROM           0xFF
 #define DEBUG_DELAY          0x7F  
-#define VEHICLE_CAR          0x01
-#define VEHICLE_TANK         0x02
 #define VEHICLE_LIGHT_STEP   0x40
 #define JOYSTICK_DATA_LENGTH 0x20
 #define JOYSTICK_DATA_START  0xAA
 #define JOYSTICK_DATA_END    0xBB
 
-#define ABS(a)               (a < 0 ? (a*(-1)) : a )
-#define MAX2(a, b)           (a >= b ? a : b)
-#define MAX(a, b, c)         (a >= b && a >= c ? a : (b >= c && b >= a ? b : c))
-#define MIN2(a, b)           (a <= b ? a : b)
-#define MIN(a, b, c)         (a <= b && a <= c ? a : (b <= c && b <= a ? b : c))
-#define MID(a, b, c)         (a <= b && a >= c ? a : (b <= c && b >= a ? b : c))
-
-//const byte VEHICLE_TYPE = VEHICLE_CAR;    // car
-const byte VEHICLE_TYPE = VEHICLE_TANK;    // tank
 
 int E1 = 4;    //PLL based M1 Speed Control
 int E2 = 7;    //PLL based M2 Speed Control
@@ -42,8 +31,6 @@ Stream* _log;
 bool useLogs       = true;
 bool useDelay      = false;
 bool usePilot      = false;
-bool blockRare     = false;
-bool blockFront    = false;
 
 // Joystick Buttons State Parts 
 bool pressed1      = false;
@@ -72,39 +59,10 @@ uint8_t receivedBytes[JOYSTICK_DATA_LENGTH];
 uint8_t v1 = 0x7F, v2 = 0x7F;
 uint8_t _b3 = 0x00, _b4 = 0x00, b3 = 0x00, b4 = 0x00;
 uint8_t _l0 = 0x00;
-uint16_t rangeFront = 0, rangeRare = 0;
 
-
-
-
-void SetServoPos(byte a, byte b)
-{
-  if(VEHICLE_TYPE == VEHICLE_CAR)
-  {
-      //TODO
-  } else
-  if(VEHICLE_TYPE == VEHICLE_TANK)
-  {
-      //TODO
-  }
-}
 
 void DriveMotorP(byte m1p, byte m2p)
-{    
-  /*
-    0x7F - engine stop
-    if(VEHICLE_TYPE == VEHICLE_CAR)
-    {
-      m1p => front-rare engine value
-      m2p => left-right engine value
-    }
-
-    if(VEHICLE_TYPE == VEHICLE_TANK)
-    {
-      m1p => (left) based on A and L (angel and length of joystick line)
-      m2p => (right) based on A and L (angel and length of joystick line)
-    }
-  */
+{   
   if(useLogs) 
   {        
     String m = "DriveMotor[";
@@ -119,49 +77,7 @@ void DriveMotorP(byte m1p, byte m2p)
     if(useDelay) delay(1000);
   }
     
-  if(VEHICLE_TYPE == VEHICLE_CAR)
-  {
-    if(m1p != 0x7F)
-    {
-        digitalWrite(E1, HIGH);
-        analogWrite(M1, (m1p));
-    } else 
-    {
-        digitalWrite(E1, LOW);
-    }
-
-    if(m2p != 0x7F)
-    {
-        int m3p = m2p > 0x7F ? 0xFF - m2p - 1 : 0xFF - m2p + 1;
-        m3p = m3p <= 0x00 ? 0x00 : m3p;  
-        m3p = m3p >= 0xFF ? 0xFF : m3p;  
-        
-        digitalWrite(E2, HIGH);
-        analogWrite(M2, (m3p));
-
-        String n = "DroveMotorP[";
-        n += String(driveMode);
-        n += "]: ";
-        n += String(m1p, HEX);
-        n += ",";
-        n += String(m3p,HEX);
-    
-        _log->println(n);
-        
-    } else
-    {
-        digitalWrite(E2, LOW);
-    }
-  } else
-  if(VEHICLE_TYPE == VEHICLE_TANK)
-  { 
-//    int16_t left  = joystick2MotorLeft(m1p, m2p);
-//    int16_t right = joystick2MotorRight(m1p, m2p);
-//    
-//    digitalWrite(E1, HIGH);
-//    analogWrite(M1, left);
-//    digitalWrite(E1, HIGH);
-//    analogWrite(M1, right);
+  
     int16_t a = m1p>=0x7F ? m1p-0x7F : 0x7F-m1p;
     int16_t b = m2p>=0x7F ? m2p-0x7F : 0x7F-m2p;
     int32_t c2 = a>b ? a*a-b*b : b*b-a*a;
@@ -256,7 +172,7 @@ void DriveMotorP(byte m1p, byte m2p)
     {
       digitalWrite(E2, LOW);
     }
-  }
+  
 }
 
 void light(uint8_t level)
@@ -284,40 +200,6 @@ void light(uint8_t level)
     analogWrite(L2, level);
 
     _l0 = level;
-}
-
-const char * vehicleType()
-{
-  switch(VEHICLE_TYPE)
-  {
-    case VEHICLE_CAR: return "Car";
-    case VEHICLE_TANK: return "Tank";
-  }
-  return "Something";
-}
-
-void setup() 
-{
-    Serial.begin(115200);
-    Serial1.begin(115200);
-
-    int i;
-    for(i=4;i<=7;i++) pinMode(i, OUTPUT);
-
-    pinMode(L1, OUTPUT);
-    pinMode(L2, OUTPUT);
-
-    pinMode(A6, INPUT);
-    pinMode(A7, INPUT);
-
-    _pad = &(Serial);
-    _log = &(Serial1);
-
-    String m = "Robo";
-    m += vehicleType();
-    m += " is ready!";
-        
-    _log->println(m);
 }
 
 uint8_t getXMove(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
@@ -350,10 +232,30 @@ uint8_t getYMove(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
   }
 }
 
+
+void setup() 
+{
+    Serial.begin(115200);
+    Serial1.begin(115200);
+
+    int i;
+    for(i=4;i<=7;i++) pinMode(i, OUTPUT);
+
+    pinMode(L1, OUTPUT);
+    pinMode(L2, OUTPUT);
+
+    pinMode(A6, INPUT);
+    pinMode(A7, INPUT);
+
+    _pad = &(Serial);
+    _log = &(Serial1);
+
+    _log->println("RoboTank is ready");
+}
+
 void loop() 
 { 
     receiveBytes(_pad);
-    //checkBlocks();
     
     if(showNewData())
     {
@@ -366,8 +268,6 @@ void loop()
         
         v1 = getYMove(a, b, c, d);
         v2 = getXMove(a, b, c, d);
-//        v1 = receivedBytes[2];
-//        v2 = receivedBytes[1];
 
         b3 = receivedBytes[6];
         b4 = receivedBytes[7];
@@ -377,15 +277,8 @@ void loop()
         if(pid == _pid)return;   
         _pid = pid;
 
-        if((v1 < 0x7F && blockRare == false)
-        || (v1 > 0x7F && blockFront == false))
-        {
-            DriveMotorP(v1, v2);
-        } else
-        {
-            DriveMotorP(0x7F, v2);    
-        }
-
+        DriveMotorP(v1, v2);
+        
         checkButtons();
 
         //Control Drive Mode
@@ -413,8 +306,6 @@ void loop()
             light(l0); 
         }
     }
-
-    //checkBlocks();
 }
 
 void checkButtons()
@@ -464,43 +355,6 @@ void checkButtons()
       
         _log->println(m);
         if(useDelay) delay(DEBUG_DELAY);
-    }
-}
-
-void checkBlocks()
-{
-    if(VEHICLE_TYPE == VEHICLE_CAR)
-    {
-      uint16_t d1 = getRangeFront();
-      uint16_t d2 = getRangeRare();
-
-      if(useLogs) 
-      {  
-          String m = "Distances: ";
-          m += String(d1, HEX);
-          m += ",";
-          m += String(d2, HEX);
-          m += " (";
-          m += (blockFront ? "blocked"  : "free");
-          m += ",";
-          m += (blockRare ? "blocked"  : "free");
-          m += ")";
-          
-          _log->println(m);
-
-          if(useDelay) delay(DEBUG_DELAY);
-      }
-    
-      if((v1 < 0x7F && blockRare)
-      || (v1 > 0x7F && blockFront))  
-      {
-          DriveMotorP(0x7F, v2);
-      }
-    } else
-    if(VEHICLE_TYPE == VEHICLE_TANK)
-    {
-      blockRare = false;
-      blockFront = false;
     }
 }
 
@@ -563,30 +417,4 @@ bool showNewData()
     }
 
     return false;
-}
-
-uint16_t getRangeFront () 
-{
-    uint16_t value = analogRead (A6);
-
-    if (value < 10) value = 10;
-    uint16_t range = ((67870.0 / (value - 3.0)) - 40.0);
-
-    rangeFront = range;
-    blockFront = rangeFront <= BLOCK_FROM;
-   
-    return range;
-}
-
-uint16_t getRangeRare () 
-{
-    uint16_t value = analogRead (A7);
-    
-    if (value < 10) value = 10;
-    uint16_t range = ((67870.0 / (value - 3.0)) - 40.0);
-
-    rangeRare = range;
-    blockRare= rangeRare<= BLOCK_FROM;
-    
-    return range;
 }
